92b46213468cdc8a1a9f73de4c6b45886bbcdaf0
Brad Korman <brad.korman@remarque-creative.com>
2019-12-26 10:56:31 +0200

    setup le wagon style

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+        })
+  )
+module.exports = environment
+// app/javascript/packs/application.js
+import 'bootstrap';
